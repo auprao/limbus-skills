81c46feb624a45c92a822bcffe7cfd370d386019
auprao <gdwotek@gmail.com>
2025-02-12 16:47:22 +0100

    added executing skills by name

--- Main.py
+++ Main.py
@@ -11,7 +11,8 @@ sanity = 0
 input_text = f"""Type 0 to exit.
 Type 1 to change current Sanity.
 Type 2 to see list of skills.
-Type 3 to execute skill by number: """
+Type 3 to execute skill by number
+Type 4 to execute skill by name: """
 running = True
 
 while running :
@@ -21,10 +22,21 @@ while running :
         case "0" :
             running = False
         case "1" :
-            sanity = int(input("Type the new sanity number, between -45 and 45: "))
+            sanity = int(input("Type the new sanity number, between -45 and 45: ")) # add error handling
         case "2" :
             read_skills()
         case "3" :
             skill_choice = int(input("Write the skill's number: "))
-            skills[skill_choice - 1].execute_skill(sanity)  # might wanna prevent errors here
+            skills[skill_choice - 1].execute_skill(sanity)  # add error handling
+        case "4" :
+            skill_choice = input("Write the skill's name: ")
+            done = False
+            for skill in skills :
+                if skill.name.lower() == skill_choice.lower() :
+                    skill.execute_skill(sanity)
+                    done = True
+                    break
+            if not done:
+                print("No skill with such name.")
+
 print("Exiting program.")
