27c349ef2f5fcb1f670bc1d687efaf1e6f6cfbe3
auprao <gdwotek@gmail.com>
2025-02-12 17:01:46 +0100

    added creating and using own custom skills :)

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from Skills import skills
+from Skills import skills, add_skill
 from Skill import Skill
 
 def read_skills() :
@@ -6,13 +6,15 @@ def read_skills() :
         print(f"{i + 1}. {skills[i].name}")
         print(f"Expected damage: {skills[i].get_expected_dmg()}")
 
+
 sanity = 0
 
 input_text = f"""Type 0 to exit.
 Type 1 to change current Sanity.
 Type 2 to see list of skills.
-Type 3 to execute skill by number
-Type 4 to execute skill by name: """
+Type 3 to add a skill of your own.
+Type 4 to execute skill by number.
+Type 5 to execute skill by name: """
 running = True
 
 while running :
@@ -26,9 +28,11 @@ while running :
         case "2" :
             read_skills()
         case "3" :
+            add_skill()
+        case "4" :
             skill_choice = int(input("Write the skill's number: "))
             skills[skill_choice - 1].execute_skill(sanity)  # add error handling
-        case "4" :
+        case "5" :
             skill_choice = input("Write the skill's name: ")
             done = False
             for skill in skills :
--- Skills.py
+++ Skills.py
@@ -13,3 +13,17 @@ skills.append(Skill("Self-destructive Purge", 30, add, -12, 3))
 skills.append(Skill("Multiply", 2, mult, 2, 3))
 skills.append(Skill("Divide", 20, div, 2, 3))
 skills.append(Skill("Imaginary Slash", complex(3, 2), mult, complex(0, 3), 4))
+
+def add_skill() :
+    name = input("Skill name: ")
+    operation = input('What type of skill? Type "add" for positive/negative, "mult" for multiplicative, "div" for dividing: ')
+    coin_count = int(input("Coin count: "))
+    base_power = int(input("Base power: "))
+    coin_power = int(input("(for negative coins, add a minus) Coin power: "))
+    match operation :
+        case "add" :
+            skills.append(Skill(name, base_power, add, coin_power, coin_count))
+        case "mult" :
+            skills.append(Skill(name, base_power, mult, coin_power, coin_count))
+        case "div" :
+            skills.append(Skill(name, base_power, div, coin_power, coin_count))
